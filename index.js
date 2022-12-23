e0b600651febaed9a104d70eea0cebf66d93bc19
usmanh25 <usmanh25@yahoo.com>
2022-12-22 23:56:15 -0800

    edit index.js

--- index.js
+++ index.js
@@ -74,20 +74,20 @@ mongoose
   .catch((error) => console.log(`${error} Check code in server/index.js`));
 
 
-// FIRST HEROKU CONFIG
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"))
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-//   })
-// }
+FIRST HEROKU CONFIG
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"))
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  })
+}
 
-// Step 1:
-app.use(express.static(path.resolve(__dirname, "./client/build")));
-// Step 2:
-app.get("*", function(request, response) {
-  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-});
+// // Step 1:
+// app.use(express.static(path.resolve(__dirname, "./client/build")));
+// // Step 2:
+// app.get("*", function(request, response) {
+//   response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+// });
 
 
 
