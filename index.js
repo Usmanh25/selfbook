2579bc687bdb856ae1c877fe799973a1ca415fd6
usmanh25 <usmanh25@yahoo.com>
2023-01-03 15:43:28 -0800

    fixing deployment bugs

--- index.js
+++ index.js
@@ -25,9 +25,15 @@ dotenv.config();
 mongoose.set('strictQuery', false);
 const app = express();
 app.use(express.json());
+
+
+
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(helmet({contentSecurityPolicy: false}));
+
+
+
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
