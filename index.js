6267975a07e4cbc668779eee8d14f0ef903406b2
usmanh25 <usmanh25@yahoo.com>
2023-01-03 14:43:55 -0800

    f

--- index.js
+++ index.js
@@ -27,11 +27,7 @@ const app = express();
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
-// app.use(
-//   helmet({
-//     contentSecurityPolicy: false,
-//   })
-// );
+app.use(helmet({contentSecurityPolicy: false}));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
