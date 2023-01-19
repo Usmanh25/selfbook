8eadac7cac3810b49f219381425f6ed98654a3cc
usmanh25 <usmanh25@yahoo.com>
2023-01-19 03:01:39 -0800

    title added

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(express.json());
 
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
-// app.use(helmet({contentSecurityPolicy: false}));
+
 app.use(helmet.contentSecurityPolicy({directives: {
   defaultSrc: ["'self'"],
   connectSrc: ["'self'", "*"],
